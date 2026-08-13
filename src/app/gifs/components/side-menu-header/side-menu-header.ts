030a595f57dfa16fb0524f0fdad93142ad171f65
Maryypp <marypvh26@gmail.com>
2026-08-12 19:30:31 -0600

    Angular Environments y Path Alias

--- src/app/gifs/components/side-menu-header/side-menu-header.ts
+++ src/app/gifs/components/side-menu-header/side-menu-header.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-
+import { environment } from '@environments/environment';
+// import { environment } from '../../../../../environments/environment.development';
 
 @Component({
   selector: 'gifs-side-menu-header',
   imports: [],
   templateUrl: './side-menu-header.html',
 })
-export class SideMenuHeader {}
+export class SideMenuHeader { 
+   envs = environment;
+}
+
