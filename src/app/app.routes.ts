bf2f84868023f77dc36f823830464585093aa170
Maryypp <marypvh26@gmail.com>
2026-08-19 23:51:09 -0600

    Argumentos dinámicos por URL

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,10 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./gifs/pages/trending-page/trending-page'),
   },
+        {
+        path: 'history/:query',
+        loadComponent: () => import('./gifs/pages/gif-history/gif-history'),
+      },
   {
     path: 'search',
     loadComponent: () =>
