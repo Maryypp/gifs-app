294378b09939421a460326a43f3a9c7d33a9b0be
Maryypp <marypvh26@gmail.com>
2026-08-19 23:57:07 -0600

    Mostrar historial de búsqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,7 +4,7 @@ import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
-import { map, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 // Record<string, Gif[]>
 
@@ -39,7 +39,7 @@ export class GifService {
         console.log({ gifs });
       });
   }
-   searchGifs(query: string) {
+    searchGifs(query: string): Observable<Gif[]> {
      return this.http
       .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
         params: {
@@ -64,6 +64,10 @@ export class GifService {
     //   const gifs = GifMapper.mapGiphyItemsTGifArray(resp.data);
     //   console.log({ search: gifs });
     // });
+  }
+
+  getHistoryGifs(query: string): Gif[] {
+    return this.searchHistory()[query] ?? [];
   }
 
 
