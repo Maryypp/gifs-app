d4752b8aa3a959cd486a9e8c9b13f9690f71db76
Maryypp <marypvh26@gmail.com>
2026-08-26 20:46:21 -0600

    Cargar siguiente página de Gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -18,8 +18,9 @@ const loadFromLocalStorage = () => {
 export class GifService {
      private http = inject(HttpClient);
 
-     trendingGifs = signal<Gif[]>([]);
-     trendingGifsLoading = signal(true);
+    trendingGifs = signal<Gif[]>([]);
+    trendingGifsLoading = signal(false);
+    private trendingPage = signal(0);
 
         trendingGifGroup = computed<Gif[][]>(() => {
     const groups = [];
@@ -46,16 +47,22 @@ export class GifService {
 
 
   loadTrendingGifs() {
+    if (this.trendingGifsLoading()) return;
+
+    this.trendingGifsLoading.set(true);
+
     this.http
       .get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
         params: {
           api_key: environment.giphyApiKey,
           limit: 20,
+          offset: this.trendingPage() * 20,
         },
       })
       .subscribe((resp) => {
         const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
-        this.trendingGifs.set(gifs);
+        this.trendingGifs.update((currentGifs) => [...currentGifs, ...gifs]);
+        this.trendingPage.update((page) => page + 1);
         this.trendingGifsLoading.set(false);
       
       });
